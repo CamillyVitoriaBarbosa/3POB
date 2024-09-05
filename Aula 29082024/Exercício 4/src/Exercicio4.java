a051dfacf5f0adcd860950f967b6bab4d5d278cf
CamillyVitoriaBarbosa <camilly.23204708360034@faeterj-rio.edu.br>
2024-09-05 09:46:47 -0300

    Update Exercicio4.java

--- Aula 29082024/Exercício 4/src/Exercicio4.java
+++ Aula 29082024/Exercício 4/src/Exercicio4.java
@@ -1,5 +1,33 @@
 package aula_29082024;
 
 public class Exercicio4 {
+   public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
 
+        System.out.print("Digite o total de games vendidos: ");
+        int totalGames = scanner.nextInt();
+        System.out.print("Digite o preço de cada game: ");
+        double precoGame = scanner.nextDouble();
+
+        System.out.print("Digite o total de calculadoras vendidas: ");
+        int totalCalculadoras = scanner.nextInt();
+        System.out.print("Digite o preço de cada calculadora: ");
+        double precoCalculadora = scanner.nextDouble();
+
+        System.out.print("Digite o total de canetas vendidas: ");
+        int totalCanetas = scanner.nextInt();
+        System.out.print("Digite o preço de cada caneta: ");
+        double precoCaneta = scanner.nextDouble();
+
+        double faturamentoGames = totalGames * precoGame;
+        double faturamentoCalculadoras = totalCalculadoras * precoCalculadora;
+        double faturamentoCanetas = totalCanetas * precoCaneta;
+        double faturamentoTotal = faturamentoGames + faturamentoCalculadoras + faturamentoCanetas;
+
+        System.out.println("Faturamento com games: R$ " + df.format(faturamentoGames));
+        System.out.println("Faturamento com calculadoras: R$ " + df.format(faturamentoCalculadoras));
+        System.out.println("Faturamento com canetas: R$ " + df.format(faturamentoCanetas));
+        System.out.println("Faturamento total: R$ " + df.format(faturamentoTotal));
+    }
 }
