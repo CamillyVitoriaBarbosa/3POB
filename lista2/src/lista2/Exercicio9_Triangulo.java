d1748bfb4d6109cfbfff1e492e847e68f6ca0c07
CamillyVitoriaBarbosa <camilly.23204708360034@faeterj-rio.edu.br>
2024-09-26 09:09:44 -0300

    Update Exercicio9_Triangulo.java

--- lista2/src/lista2/Exercicio9_Triangulo.java
+++ lista2/src/lista2/Exercicio9_Triangulo.java
@@ -5,35 +5,28 @@ import java.util.Scanner;
 public class Exercicio9_Triangulo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		/* package exercicios2;
-
-import java.util.Scanner;
-
-public class ex09 {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Entre o comprimento do lado X: ");
-    double x = scanner.nextDouble();
-    System.out.print("Entre o comprimento do lado Y: ");
-    double y = scanner.nextDouble();
-    System.out.print("Entre o comprimento do lado X: ");
-    double z = scanner.nextDouble();
-
-    if (x + y > z && x + z > y && y + z > x) {
-      if (x == y && y == z) {
-        System.out.println("triângulo equilátero.");
-      } else if (x == y || x == z || y == z) {
-        System.out.println("triângulo isósceles.");
-      } else {
-        System.out.println("triângulo escaleno.");
-      }
-    } else {
-      System.out.println("Os valores fornecidos não formam um triângulo.");
+	Scanner scanner = new Scanner(System.in);
+		
+	System.out.println("Digite os comprimentos dos lados do triângulo: ");
+	System.out.println("Lado X: ");
+	int ladoX = scanner.nextInt();
+	System.out.print("Lado Y: ");
+	int ladoY = scanner.nextInt();
+	System.out.print("Lado Z: ");
+	int ladoZ = scanner.nextInt();
+	   
+	if ((ladoX < ladoY + ladoZ) && (ladoY < ladoX + ladoZ) && (ladoZ < ladoX + ladoY)) {
+	    if (ladoX == ladoY && ladoY == ladoZ) {
+		System.out.println("O triângulo é equilátero");
+	    } else if (ladoX == ladoY || ladoY == ladoZ || ladoZ == ladoX) {
+		System.out.println("O triângulo é isósceles");
+	    } else {
+		System.out.println("O triângulo é escaleno");
+	    }
+        } else {
+            System.out.println("Os lados não formam um triângulo.");
+        }
+	    
+	scanner.close();
     }
-  }
-
-} */
-	}
-
 }
