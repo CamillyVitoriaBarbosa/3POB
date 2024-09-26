c5057bc0cdb1dfae8a4f6e08af3fd10c949f0294
CamillyVitoriaBarbosa <camilly.23204708360034@faeterj-rio.edu.br>
2024-09-26 08:49:32 -0300

    Update Exercicio8_FormaDPagamento.java

--- lista2/src/lista2/Exercicio8_FormaDPagamento.java
+++ lista2/src/lista2/Exercicio8_FormaDPagamento.java
@@ -1,69 +1,49 @@
 package lista2;
 
+import java.util.Scanner;
+
 public class Exercicio8_FormaDPagamento {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		/*import java.util.Scanner;
-
-public class Exercicio8 {
-
-    // 8. Faça um programa que leia o código, a quantidade,
-    // o preço unitário e a forma de pagamento
-    // (1 - Dinheiro, 2 - Cheque, 3 - Cartão débito, 4 - Cartão crédito)
-    // de um produto. Pagamentos em dinheiro recebem um desconto de 10%,
-    // em cartão de débito 5%, em cartão de crédito 3% e em cheque 2%.
-    // O programa deve informar o valor do desconto e o valor final a pagar.
-
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 
         System.out.print("Digite o codigo do produto: ");
         int codigo = scanner.nextInt();
         System.out.print("Digite a quantidade de produto(s): ");
-        int quantidade = scanner.nextInt();
+        int qnt = scanner.nextInt();
         System.out.print("Digite o preço unitário do produto: ");
         double preco = scanner.nextDouble();
-        System.out.println("Digite o forma de pagamento:");
+        System.out.println("Digite o forma de pagamento");
         System.out.println("1 - Dinheiro\t2 - Cheque\t3 - Cartão débito\t4 - Cartão crédito");
-        int formaDePagamento = scanner.nextInt();
+        int opPagamento = scanner.nextInt();
 
-        preco *= quantidade;
+        preco *= qnt;
        
-        switch (formaDePagamento) {
+        switch (opPagamento) {
             case 1:
-                preco *= 0.9;
+                preco -= (preco*0.10);
                 break;
 
             case 2:
-                preco *= 0.98;
+                preco -= (preco*0.02);
                 break;
 
             case 3:
-                preco *= 0.95;
+                preco -= (preco*0.05);
                 break;
 
             case 4:
-                preco *= 0.97;
+                preco -= (preco*0.03);
                 break;
 
             default:
-                System.out.println("ERROR");
+                System.out.println("Forma de pagamento inválida!");
                 break;
         }
 
-        System.out.println("preco final:" + preco);
+        System.out.println("Preço total:" + preco);
 
         scanner.close();
-
-    }
-   
-
-
-
-}*/
-
 	}
 
 }
