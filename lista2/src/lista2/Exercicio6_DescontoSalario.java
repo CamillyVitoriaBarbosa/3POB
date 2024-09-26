d191dacbef5626ae7f6c149253ddc097d9c343fb
CamillyVitoriaBarbosa <camilly.23204708360034@faeterj-rio.edu.br>
2024-09-26 09:14:10 -0300

    Update Exercicio6_DescontoSalario.java

--- lista2/src/lista2/Exercicio6_DescontoSalario.java
+++ lista2/src/lista2/Exercicio6_DescontoSalario.java
@@ -6,37 +6,36 @@ import java.text.DecimalFormat;
 public class Exercicio6_DescontoSalario {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+	Scanner scanner = new Scanner(System.in);
         DecimalFormat df = new DecimalFormat("0.00");
 		
         System.out.println("Digite o seu nome: ");
         String nome = scanner.nextLine();
-		System.out.println("Digite o valor do salário mínimo: ");
+	System.out.println("Digite o valor do salário mínimo: ");
         double salarioMinimo = scanner.nextDouble();
         System.out.print("Digite o seu salário: ");
         double salario = scanner.nextDouble();
         
         if (salario<salarioMinimo) {
-        	double desconto = salario* 0.02;
-        	double salarioLiquido = salario - desconto;
+           double desconto = salario* 0.02;
+           double salarioLiquido = salario - desconto;
+        
+	   System.out.println("O valor do seu desconto é de: " + df.format(desconto));
+           System.out.println("O seu salário final é: " + df.format(salarioLiquido));
+	} else if (salarioMinimo<salario) {
+	   double desconto = salario* 0.08;
+           double salarioLiquido = salario - desconto;
         	
-        	System.out.println("O valor do seu desconto é de: " + df.format(desconto));
-            System.out.println("O seu salário final é: " + df.format(salarioLiquido));
-	    } else if (salarioMinimo<salario) {
-	    	double desconto = salario* 0.08;
-        	double salarioLiquido = salario - desconto;
+           System.out.println("O valor do seu desconto é de: " + df.format(desconto));
+           System.out.println("O seu salário final é: " + df.format(salarioLiquido));
+	} else {
+	   double desconto = salario* 0.05;
+           double salarioLiquido = salario - desconto;
         	
-        	System.out.println("O valor do seu desconto é de: " + df.format(desconto));
-            System.out.println("O seu salário final é: " + df.format(salarioLiquido));
-	    } else {
-	    	double desconto = salario* 0.05;
-        	double salarioLiquido = salario - desconto;
-        	
-        	System.out.println("O valor do seu desconto é de: " + df.format(desconto));
-            System.out.println("O seu salário final é: " + df.format(salarioLiquido));
-	    }
-
-        scanner.close();
+           System.out.println("O valor do seu desconto é de: " + df.format(desconto));
+           System.out.println("O seu salário final é: " + df.format(salarioLiquido));
 	}
-
+		
+        scanner.close();
+    }
 }
