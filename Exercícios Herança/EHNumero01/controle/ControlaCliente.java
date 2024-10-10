d0586c4ac973bced90e3be084adcbee4db974090
CamillyVitoriaBarbosa <camilly.23204708360034@faeterj-rio.edu.br>
2024-10-10 11:54:31 -0300

    Update ControlaCliente.java

--- Exercícios Herança/EHNumero01/controle/ControlaCliente.java
+++ Exercícios Herança/EHNumero01/controle/ControlaCliente.java
@@ -1,5 +1,12 @@
 package controle;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import dominio.Cliente;
+import dominio.ClientePessoaFisica;
+import dominio.ClientePessoaJuridica;
+
 public class ControlaCliente {
 	public static void main(String[] args) {
 		
